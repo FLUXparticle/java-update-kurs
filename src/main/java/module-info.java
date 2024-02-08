e0d70d8eff5b490e9395bf6f08f2ce63f4e939eb
Sven Reinck <sreinck@fluxparticle.de>
2024-02-08 04:33:22 +0100

    JDK17 Korrektur

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module java.update.kurs {
-    requires jdk.incubator.vector;
     requires jdk.httpserver;
     requires json.simple;
     requires java.sql;
     requires java.net.http;
+    requires org.slf4j;
 }
